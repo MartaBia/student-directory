42e4a8c5e1bb854b2a953269da0e76070e377716
Marta Bianchini <marta.bianchini24@gmail.com>
2022-11-30 17:33:41 +0000

    Changed methods positions

--- directory.rb
+++ directory.rb
@@ -1,9 +1,35 @@
 @students = []
 
+def input_students
+  puts "Please, enter the name of the student"
+  puts "To finish, just hit return twice"
+
+  name = gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
+  end 
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list(@students)
+  print_footer(@students)
 end
 
 def process(selection)
@@ -19,38 +45,12 @@ def process(selection)
   end
 end
 
-def show_students
-  print_header
-  print_names(@students)
-  print_footer(@students)
-end
-
-def interactive_menu
-  loop do 
-    print_menu
-    process(gets.chomp)
-  end 
-end
-
-def input_students
-  puts "Please, enter the name of the student"
-  puts "To finish, just hit return twice"
-
-  name = gets.chomp
-
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
-
 def print_header
   puts "The students of Villains Academy"
   puts "---------------------"
 end
 
-def print_names(array)
+def print_student_list(array)
   array.each do |hash|
     puts "#{hash[:name]} (#{hash[:cohort]} cohort)"
   end
