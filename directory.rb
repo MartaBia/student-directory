df2ac4776b30f2f836481b519c8655cb49f45656
Marta Bianchini <marta.bianchini24@gmail.com>
2022-12-02 11:32:49 +0000

    Added try_load_students method and updated gets() method with STDIN

--- directory.rb
+++ directory.rb
@@ -4,19 +4,19 @@ def input_students
   puts "Please, enter the name of the student"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do 
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end 
 end
 
@@ -77,12 +77,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv") #this is a default value, if no argument is passed then the value "students.csv" will be used instead
+  file = File.open(filename, "r")
 
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students.push(name: name, cohort: cohort.to_sym)
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exist?(filename) #if it's given and it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} does not exists."
+    exit
   end
 end
 
