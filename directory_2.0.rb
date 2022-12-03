149a2ba98d1c9a6b32c86123453bcc3656528369
Marta Bianchini <marta.bianchini24@gmail.com>
2022-12-03 01:49:48 +0000

    Added enter_students_details method and capitalised user's inputs

--- directory_2.0.rb
+++ directory_2.0.rb
@@ -4,7 +4,7 @@ def input_students
   students = []
 
   while true
-    puts "*********************".center(30)
+    # puts "*********************".center(30) ALREADY ADDED
     puts "Please, enter the name of the student"
     puts "To finish, just hit return twice"
     name = gets.chomp
@@ -26,11 +26,12 @@ def input_students
 
     students.push({name: name, country_of_birth: country_of_birth, cohort: cohort})
 
-    if students.length == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{students.count} students"
-    end
+    # ADDED
+    # if students.length == 1
+    #   puts "Now we have 1 student"
+    # else
+    #   puts "Now we have #{students.count} students"
+    # end
   end
 
   students
@@ -38,46 +39,55 @@ end
 
 def print_header
   puts "The students of Villains Academy"
-  puts "---------------------".center(30)
+  # puts "---------------------".center(30) ADDED
+end
+
+#TODO: copy search_for_letters method and modify print names on directory 2.0.1
+
+def search_for_letters
+  puts "Would you like to search names for a specific letter?"
+  puts "Write 'Yes' if you would, otherwise just hit return"
+  letter_search_answer = gets.chomp.downcase
+
+  return if letter_search_answer.empty?
+
+  if letter_search_answer == "yes"
+    puts "Which letter would you like to search for?"
+    letter = gets.chomp.downcase
+  end
+
+  letter
 end
 
 def print_names(array)
   if !array.empty?
-    puts "Would you like to search names for a specific letter?"
-    puts "Write 'Yes' if you would, otherwise just hit return"
-    letter_search_answer = gets.chomp.downcase
-
-    if letter_search_answer == "yes"
-      puts "Which letter would you like to search for?"
-      letter = gets.chomp.downcase
-    end
-
     array.each_with_index do |hash, i|
       name_first_letter = (hash[:name])[0].downcase
 
-    
-      if letter == name_first_letter
-        puts "- #{(hash[:name]).capitalize}, country of birth: #{(hash[:country_of_birth].capitalize)} (#{hash[:cohort].capitalize} cohort)"
-        
-      end
-      if letter_search_answer.empty?
+      
+      if search_for_letters == nil
         puts "#{i + 1} #{(hash[:name]).capitalize}, country of birth: #{(hash[:country_of_birth].capitalize)} (#{hash[:cohort].capitalize} cohort)"
+      else
+        if name_first_letter == search_for_letters
+          puts "- #{(hash[:name]).capitalize}, country of birth: #{(hash[:country_of_birth].capitalize)} (#{hash[:cohort].capitalize} cohort)"
+        end
       end
     end
   end
 end
 
-def print_footer(array)
-  if array.empty?
-    puts "No students here"
-  else
-    if array.length == 1
-      puts "We only have 1 (but great) student"
-    else
-      puts "Overall, we have #{array.count} great students"
-    end
-  end
-end
+# ADDED
+# def print_footer(array)
+#   if array.empty?
+#     puts "No students here"
+#   else
+#     if array.length == 1
+#       puts "We only have 1 (but great) student"
+#     else
+#       puts "Overall, we have #{array.count} great students"
+#     end
+#   end
+# end
 
 students = input_students
 print_header
